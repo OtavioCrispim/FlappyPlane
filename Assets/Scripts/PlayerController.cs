cc8b9d885fe8be4cb853de013b28a5e3ca5e0311
Otávio Crispim <otavio.guilherme.crispim@gmail.com>
2025-05-28 00:00:14 -0300

    Limitando a velocidade de queda do player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]private Rigidbody2D rb;
+    private Rigidbody2D rb;
+    [SerializeField]private float velocidade = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.velocity = Vector2.up * velocidade;
+
+        if(rb.velocity.y < -velocidade)
+        {
+            rb.velocity = Vector2.down * velocidade;
+        }
     }
 }
